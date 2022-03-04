0f2b90cbc347f43bf7026ba454d04899c791a06e
BuildTools <unconfigured@null.spigotmc.org>
2022-03-03 19:36:30 -0600

    fixed a bug from a outdated json file

--- Bot.py
+++ Bot.py
@@ -2,21 +2,7 @@ import random
 
 import discord  # 40% yes 40% no 20% random
 from discord.ext import commands
-import json
 import os
-
-if os.path.exists(os.getcwd() + "/config.json"):
-
-    with open("config.json") as f:
-        configData = json.load(f)
-else:
-    configTemplate = {"Token": "", "Prefix": "c."}
-
-    with open(os.getcwd() + "/config.json", "w+") as f:
-        json.dump(configTemplate, f)
-
-token = configData["Token"]
-prefix = configData["Prefix"]
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix="c.")
